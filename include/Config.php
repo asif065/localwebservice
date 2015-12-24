886b09594399a85dd7c1cfdd6f5df2052420f070
Khondoker Asif Akter <Khondoker Asif Akter>
2015-12-25 00:36:36 +0600

    initial version

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "asif");
+define("DB_PASSWORD", "asif065");
+define("DB_DATABASE", "android_api");
+?>
